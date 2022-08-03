b4c47b4133a8d255fc9ab488ee32a302ecf90530
TaiLe <Taic21a.th1@gmail.com>
2022-08-03 20:10:53 +0700

    Backup

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,11 +7,10 @@ const audio = $('#audio');
 const btnPrev = $('.prev');
 const btnNext = $('.next');
 
-// const songs = ['1', '2'];
-const songs = [{ url: 1 }, { url: 2 }];
-var songIndex = 0;
+const songs = ['1', '2'];
+let songIndex = 0;
 
-loadSong(songs[songIndex].url);
+loadSong(songs[songIndex]);
 
 function loadSong(song) {
   audio.src = `./assets/sounds/${song}.mp3`;
@@ -28,10 +27,22 @@ const handlePlayMusic = () => {
   }
 };
 const handleNextMusic = () => {
-  songIndex = songIndex + 1;
-  loadSong(songs[songIndex].url || songs[0].url);
+  songIndex++;
+  if (songIndex >= songs.length) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  audio.play();
+};
+const handlePrevMusic = () => {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
   audio.play();
 };
 btnPlay.addEventListener('click', handlePlayMusic);
 btnPause.addEventListener('click', handlePlayMusic);
 btnNext.addEventListener('click', handleNextMusic);
+btnPrev.addEventListener('click', handlePrevMusic);
